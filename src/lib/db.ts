ef0edaa0970e66f2e72057d1599977a917af8da1
rohittcodes <rohittcodes@gmail.com>
2024-12-15 14:11:32 +0530

    refactor: edge-adaption

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,11 @@
-import { PrismaClient } from "@prisma/client"
- 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
- 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
- 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+import { Pool, neonConfig } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaClient } from '@prisma/client'
+import ws from 'ws'
+
+neonConfig.webSocketConstructor = ws
+const connectionString = `${process.env.DATABASE_URL}`
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaNeon(pool)
+export const prisma = new PrismaClient({ adapter })
